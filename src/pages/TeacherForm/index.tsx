aaa82e3e43b3e80c002789acfc3de626f0af5690
DeividMartins <deividmartins8902@gmail.com>
2020-08-12 23:28:50 -0300

    Comitando o curso do nlw 2

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrivel que você quer dar aulas." >
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject" />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da semana</label>
+                        <input type="text" id="subject" />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="subject" />
+                    </div>
+                </form>
+            </PageHeader>
+        </div>
+    )
+}
+
+export default TeacherForm;
